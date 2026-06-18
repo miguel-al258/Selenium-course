63c47dff43fe41406e4e57be8637b6c68c937a0e
Miguel Lopez <miguelal258@gmail.com>
2026-06-18 14:43:07 -0600

    Se agrega Test6

---
+++ Test6_Implicit_Wait.py
@@ -0,0 +1,30 @@
+from selenium import webdriver
+import time
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+
+
+driver=webdriver.Chrome()
+t=2
+
+
+driver.get("https://demoqa.com/text-box")
+driver.maximize_window()
+driver.implicitly_wait(10)
+time.sleep(t)
+
+nom = driver.find_element(By.XPATH, "//input[@id='userName']")
+nom.send_keys("Miguel")
+time.sleep(t)
+driver.find_element(By.XPATH, "//input[@id='userEmail']").send_keys("miguel@gmail.com")
+time.sleep(t)
+driver.find_element(By.XPATH,"//textarea[@id='currentAddress']").send_keys("Calle1")
+time.sleep(t)
+driver.find_element(By.XPATH, "//textarea[@id='permanentAddress']").send_keys("Calle2")
+time.sleep(t)
+driver.execute_script("window.scrollTo(0,300)")
+driver.find_element(By.XPATH, "//button[@id='submit']").click()
+time.sleep(t)
+
+
+driver.close()
